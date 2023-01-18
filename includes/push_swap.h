ae0e64184f2f0c3a81879b2854793f258a52f10f
zstenger <zstenger@student.42heilbronn.de>
2023-01-18 12:15:02 +0100

    cleaned up

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,19 +6,17 @@
 /*   By: zstenger <zstenger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 17:15:21 by zstenger          #+#    #+#             */
-/*   Updated: 2023/01/18 11:45:06 by zstenger         ###   ########.fr       */
+/*   Updated: 2023/01/18 12:14:01 by zstenger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
-# include <unistd.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <limits.h>
+# include <unistd.h>
 # include "../libft/libft.h"
-# define MIN "-2147483648"
-# define MAX "2147483647"
 
 typedef struct s_calculation
 {
@@ -27,45 +25,43 @@ typedef struct s_calculation
 	int	next;
 }t_calculation;
 
-void	error(void);
-
 //check the input and create a list
 int		list_size(int argc, char **argv);
-void	create_list(int length, int *list_a, char **argv);
-void	check_for_doubles(int *list, int length);
 int		list_is_in_order(int *list, int length);
+void	check_for_doubles(int *list, int length);
+void	create_list(int length, int *list_a, char **argv);
 
 //pick a sorting method
 void	speed_is_life(int *a, int *b, int length);
 
 //small sort
-void	sort_smaller(int *a, int length);
 void	sort_3(int *a);
 void	sort_5(int *a, int length);
+void	sort_smaller(int *a, int length);
 
 //ra, rb, rr, rra, rrb, rrr, sa, sb, ss
-void	ra_or_rra(int *list, int length, int max);
+void	sa_or_sb(int *list, int swap);
 void	ra_or_rb(int *list, int length);
 void	rra_or_rrb(int *list, int length);
+void	ra_or_rra(int *list, int length, int max);
 void	rb_or_rrb(int *list, int b_length, int max);
-void	sa_or_sb(int *list, int swap);
 
 //finding nemo, I mean numbers
-int		get_smallest_number(int *list, int length);
 int		get_biggest_number(int *list, int length);
+int		get_smallest_number(int *list, int length);
 int		get_next_biggest_number(int *list, int length);
 
 //big sort
+void	sort_to_a(int *list, int b_length);
 void	sort_bigger(int *a, int *b, int length);
+int		is_pivot_here(int *list, int length, int mid);
 int		pivot_finder(int *list, int b_length, int length);
 int		calculate_position(int *list, int mid, int b_length, int length);
-int		is_pivot_here(int *list, int length, int mid);
-void	sort_to_a(int *list, int b_length);
 
 //checker
 char	*read_terminal(char *operation_list);
+char	*next_operation(char *operation_list);
 void	is_valid_operation(char *operation, char *operation_list);
 void	execute_operations(char *operation_list, int *a, int *b, int b_len);
-char	*next_operation(char *operation_list);
 
 #endif
